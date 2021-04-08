0cf8204d150f07dd0eb8c9cab4520c1fc1563615
Max <myEmail@example.com>
2021-04-09 02:26:37 +0300

    Setting changed

--- NewsMVP/settings.py
+++ NewsMVP/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET KEY')
+SECRET_KEY = "django-insecure-kv_)qk86+sxqdoc*&k)katwp*_66)%(__(%y&-0sm$&11%%696"
+# os.environ.get('SECRET KEY')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
