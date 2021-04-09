d6293c370822001fdb45d4f5369dd6922e9d48d2
Max <myEmail@example.com>
2021-04-09 04:13:02 +0300

    API updated

--- api/serializers.py
+++ api/serializers.py
@@ -22,3 +22,14 @@ class NewsSerializer(serializers.ModelSerializer):
             "author_name",
             "comments",
         ]
+
+    def update(self, instance, validated_data):
+        fields = instance._meta.fields
+        exclude = []
+        for field in fields:
+            field = field.name.split('.')[-1]
+            if field in exclude:
+                continue
+            exec("instance.%s = validated_data.get(field, instance.%s)" % (field, field))
+        instance.save()
+        return instance
